91e717627240748e0391afa159c8e1e10f4c757f
Alvaro <celistony@yahoo.com>
2021-11-03 01:05:18 -0500

    Post and routes have been created...

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Categoria;
 
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
     //
+
+    public function store(Request $request) {
+        $categoria = new Categoria();
+        $categoria->nombre = $request->nombre;
+        $categoria->estado = $request->estado;
+
+        $categoria->save();
+    }
 }
--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Cliente;
 
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
     //
+
+    public function store(Request $request) {
+        $cliente = new Cliente();
+        $cliente->tipo_documento = $request->tipo_documento;
+        $cliente->numero_documento = $request->numero_documento;
+        $cliente->nombres = $request->nombres;
+        $cliente->apellidos = $request->apellidos;
+        $cliente->telefono = $request->telefono;
+        $cliente->direccion = $request->direccion;
+        $cliente->email = $request->email;
+        $cliente->estado = $request->estado;
+
+        $cliente->save();
+    }
 }
